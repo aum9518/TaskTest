b692a1333d8627720eb30c272620d0a69c85595d
aum9518 <abduganiuulumanasbek@gmail.com>
2023-03-08 17:59:28 +0600

    add TaskTest

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+        //TODO Тест тапшыруу программасын жазыныз.
+        // Тест деген класста final String[5]  массивдин ичинде 5
+        // суроо жана ар бир суроого 3 варианттары бар. Ошол эле класста жооптору
+        // бар ошол суроолорго( поле массив) .
+        //Метод жазыныз тесттенОтту деген, ар бир суроо варианттары менен крнсольго чыгып
+        // анан сизден ушул 3 варианттын бирин танда деген соз чыксын(a, b, c)
+        // эгер сиз башка эле вариант тандасаныз, мисалы j же s анда программаныз ошибка
+        // ыргытсын «туура танда» деп. Суроолорду баарына жооп берген сон сиздин баллынызды
+        // санап чыгарып берсин( при подсчете баллов, программа должна считать по 100 балльной шкале).
+
+
+        String answers1 = "Peaksof 2019 - jyly negidelgen.";
+        String answers2 = "Kyrgyz Respublikasynyn gerbi 1994 - 01-14 kabyl alyngan.";
+        String answers3 = "Kyrgyz Respublikasynyn jelegi 1992-03-03 jyly kabyl alyngan";
+        String answers4 = "Kyrgyz Respublikasynyn gimni 1991-01-18 jyly kabyl alyngan";
+        String answers5 = "Bishkek shaary 1825-jyly negidelgen";
+        String[] tests = {answers1,answers2,answers3,answers4,answers5};
+        Test test = new Test(tests);
+        test.test();
+        for (String a:tests) {
+            System.out.println("Answers: "+a);
+        }
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class Test {
+import java.util.Scanner;
+
+public class Test {
+    private final String[] test ;
+
+    public Test(String[] test) {
+        this.test = test;
+    }
+    public void test() {
+
+            try {
+                String a = "a";
+                String b = "b";
+                String c = "c";
+                int point = 0;
+                    System.out.println("Peaksoft kaisy jyly negizdelgen?: \na)2017  b)2018  c)2019");
+                    String option = new Scanner(System.in).nextLine();
+                    if (c.equals(option)) {
+                        point += 20;
+                    } else if (option.equals(a)) {
+                    } else if (option.equals(b)) {
+
+                    } else {
+                        throw new Exception("Myndai variant jok");
+
+                    }
+                System.out.println("Kyrgyz Respublikasynyn gerbi kachan kabyl alyngan?\na)1992-03-14    b)1994-01-14    c)1991-05-03");
+                String option1 = new Scanner(System.in).nextLine();
+                    if (a.equals(option1)){
+
+                    } else if (b.equals(option1)) {
+                        point+=20;
+                    } else if (c.equals(option1)) {
+
+                    }else {
+                        throw new Exception("Myndai variant jok");
+                    }
+                System.out.println("Kyrgyz Respublikasynyn jelegi kachan kabyl alyngan? \na)1991-01-12      b)1993-03-15        c)1992-03-03");
+                String option2 = new Scanner(System.in).nextLine();
+                    if (a.equals(option2)){
+
+                    } else if (b.equals(option2)) {
+
+                    } else if (c.equals(option2)) {
+                        point+=20;
+                    }else {
+                        throw new Exception("Mynday variant jok");
+                    }
+                System.out.println("Kyrgyz Respublikasynyn gimni kachan kabyl alyngan? \na)1992-12-18       b)1991-01-18        c)1993-03-15");
+                String option3 = new Scanner(System.in).nextLine();
+                    if (a.equals(option3)){
+
+                    } else if (b.equals(option3)) {
+                        point+=20;
+                    } else if (c.equals(option3)) {
+
+                    }else {
+                        throw new Exception("Myndai variant jok");
+                    }
+                System.out.println("Bishkek shaary kachan negidelgen?\na)1825-j       b)1889-j      c)1925-j");
+                String option4 = new Scanner(System.in).nextLine();
+                if (a.equals(option4)){
+                    point+=20;
+                } else if (b.equals(option4)) {
+
+                } else if (c.equals(option4)) {
+
+                }else {
+                    throw new Exception("Myndai variant jok");
+                }
+                System.out.println("You got "+point+" from 100.");
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+    }
 }
